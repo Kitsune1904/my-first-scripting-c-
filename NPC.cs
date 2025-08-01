5c039af62b7636f80590668b03c6efec43fad568
Anna <138930718+Kitsune1904@users.noreply.github.com>
2025-08-01 14:27:11 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    public int health = 10;
+    public int level = 1;
+    public float speed = 1.2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
